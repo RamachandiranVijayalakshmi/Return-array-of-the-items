8625701d7c7ea3c3e1378a70cd6b99b2525c41d0
RamachandiranVijayalakshmi <66218468+RamachandiranVijayalakshmi@users.noreply.github.com>
2020-07-18 23:37:44 +0530

    Add files via upload

---
+++ Get values.py
@@ -0,0 +1,8 @@
+def num(n):
+    print('enter the',n,'employee names')
+    for i in range(0,n):
+        b=input()
+    else:
+        print('Welcome Guys')
+a=int(input('How many employee in office:'))
+num(a)
